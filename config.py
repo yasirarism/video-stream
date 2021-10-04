2dd46c5b89297a01324334aaa771e2444ca23a3b
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-04 14:16:36 +0700

    prepare

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ if path.exists("local.env"):
 
 load_dotenv()
 
-class Veez(object):
-        admins = {}
-        BOT_TOKEN = getenv("BOT_TOKEN", None)
-        CHANNEL = int(os.getenv('CHANNEL', 123456))
-        API_ID = int(getenv("API_ID", "6"))
-        API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-        SESSION_NAME = getenv("SESSION_NAME", None)
-        DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
-        SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-        ASSISTANT_NAME = getenv("ASSISTANT_NAME", "tg_video_stream")
-        BOT_USERNAME = getenv("BOT_USERNAME", "veezvidstreambot")
-        COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-        CHANNEL_NAME = getenv("CHANNEL_NAME", "levinachannel")
-        GROUP_NAME = getenv("GROUP_NAME", "VeezSupportGroup")
-        OWNER_NAME = getenv("OWNER_NAME", "dlwrml")
+admins = {}
+PY_LIB_CALL = {}
+TG_LIB_CALL = {}
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+API_ID = int(getenv("API_ID", "6"))
+API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+SESSION_NAME = getenv("SESSION_NAME", None)
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "tg_video_stream")
+BOT_USERNAME = getenv("BOT_USERNAME", "veezvidstreambot")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+CHANNEL_NAME = getenv("CHANNEL_NAME", "levinachannel")
+GROUP_NAME = getenv("GROUP_NAME", "VeezSupportGroup")
+OWNER_NAME = getenv("OWNER_NAME", "dlwrml")
